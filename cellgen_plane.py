5303bcfa18c5c72602cebf06229573ae7c8d1f4d
MatteoRenatoCalcagni <matteorenato.calcagni@studenti.unipd.it>
2026-01-29 08:59:44 +0100

    I made the flat version with a padding of all false values. Now it is complete

--- cellgen_plane.py
+++ cellgen_plane.py
@@ -11,37 +11,29 @@ cells = np.random.choice(a=[True, False], size=(nrows, ncols))
 
 #Extracts the indexes of living cells (True) and dead cells (False)
 #I add [1, 1] in order to traslate the indexes and make them compatible with the padded matrix
-alive_idx = np.argwhere(cells == True) 
-dead_idx = np.argwhere(cells == False)
+alive_idx = np.argwhere(cells == True) + np.array([1, 1])
+dead_idx = np.argwhere(cells == False) + np.array([1, 1])
 
-#Extract border cells
-alive_idx_border = alive_idx[(alive_idx[:, 0] == 0) | (alive_idx[:, 1] == 0) 
-                             | (alive_idx[:, 0] == nrows-1) | (alive_idx[:, 1] == ncols-1)]
-dead_idx_border = dead_idx[(dead_idx[:, 0] == 0) | (dead_idx[:, 1] == 0)
-                           | (dead_idx[:, 0] == nrows-1) | (dead_idx[:, 1] == ncols-1)]
+#This create a padded matrix, with all false values as border. With it, the GoF algorithm works for a flat space
+padded = np.pad(cells, pad_width=1, mode='constant', constant_values=False)
 
-#Extracts the insider cells
-alive_idx_inside = alive_idx[(alive_idx[:, 0] != 0) & (alive_idx[:, 1] != 0)
-                             & (alive_idx[:, 0] != nrows-1) & (alive_idx[:, 1] != ncols-1)]
-dead_idx_inside = dead_idx[(dead_idx[:, 0] != 0) & (dead_idx[:, 1] != 0)
-                           & (dead_idx[:, 0] != nrows-1) & (dead_idx[:, 1] != ncols-1)]
-
-#Make a copy of the grid, i.e. the next generation
+#Make a copy of the grid, i.e. the next generation (necessary to make the code work)
 newgen = np.copy(cells)
 
-#Evolution of the alive insider cells 
-for index in alive_idx_inside:
-    neig = cells[index[0]-1:index[0]+2, index[1]-1:index[1]+2]
+#Calculates the evolution of the living cells
+for index in alive_idx:
+    neig = padded[index[0]-1:index[0]+2, index[1]-1:index[1]+2]
     #The -1 is to delete the cell I am considering from the counts of alive neighbors
     nalive = len(neig[neig == True]) - 1
     if (nalive != 2 and nalive != 3):
-        newgen[index[0], index[1]] = False
+        newgen[index[0]-1, index[1]-1] = False
 
-#Evolution of the dead insider cells
-for index in dead_idx_inside:
-    neig = cells[index[0]-1:index[0]+2, index[1]-1:index[1]+2]
-    print(neig)
+#Calculates the evolution of the dead cells
+for index in dead_idx:
+    neig = padded[index[0]-1:index[0]+2, index[1]-1:index[1]+2]
     #The -1 is to delete the cell I am considering from the counts of alive neighbors
-    nalive = len(neig[neig == True])
+    nalive = len(neig[neig == True]) 
     if (nalive == 3):
-        newgen[index[0], index[1]] = True
+        newgen[index[0]-1, index[1]-1] = True
+
+print(cells, '\n\n', newgen)
